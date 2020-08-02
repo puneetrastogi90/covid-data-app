90749451e241c5de260ca4dcb46f73c35eeb23a5
Puneet Rastogi <puneet.rastogi@rembrandtglobal.com>
2020-08-02 13:42:39 +0530

    CountryDetail fragment name changed.

--- app/src/main/java/com/covid/covidapp/fragments/CountryDetailFragment.kt
+++ app/src/main/java/com/covid/covidapp/fragments/CountryDetailFragment.kt
@@ -1,16 +1,13 @@
 package com.covid.covidapp.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.covid.covidapp.R
 import com.covid.covidapp.base.BaseFragment
 
 private const val COUNTRY_CODE = "COUNTRY_CODE"
 
-class CountryDetail : BaseFragment() {
+class CountryDetailFragment : BaseFragment() {
     private var param1: String? = null
 
 
@@ -23,12 +20,12 @@ class CountryDetail : BaseFragment() {
             param1 = it.getString(COUNTRY_CODE)
         }
     }
-    
+
     companion object {
 
         @JvmStatic
         fun newInstance(param1: String) =
-            CountryDetail().apply {
+            CountryDetailFragment().apply {
                 arguments = Bundle().apply {
                     putString(COUNTRY_CODE, param1)
                 }
