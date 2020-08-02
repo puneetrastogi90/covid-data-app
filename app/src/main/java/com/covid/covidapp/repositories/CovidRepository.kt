47dcec3ef026caee8f494639a60876f57952eecc
Puneet Rastogi <puneet.rastogi@rembrandtglobal.com>
2020-08-02 17:45:49 +0530

    Methods for data added to repository

--- app/src/main/java/com/covid/covidapp/repositories/CovidRepository.kt
+++ app/src/main/java/com/covid/covidapp/repositories/CovidRepository.kt
@@ -1,6 +1,10 @@
 package com.covid.covidapp.repositories
 
 
+import com.covid.covidapp.data.CountriesListModel
+import com.covid.covidapp.data.CountryCovidDetailModel
+import com.covid.covidapp.data.Result
+import com.covid.covidapp.data.WorldReportModel
 import com.covid.covidapp.datasources.CovidLocalDataSource
 import com.covid.covidapp.datasources.CovidRemoteDataSource
 import javax.inject.Inject
@@ -10,6 +14,20 @@ class CovidRepository @Inject constructor(
     private val covidLocalDataSource: CovidLocalDataSource
 ) {
 
+    suspend fun getWorldData(): Result<WorldReportModel, WorldReportModel> {
+        val result = covidRemoteDataSource.getWorldCovidData()
+        return result
+    }
+
+    suspend fun getCountriesList(): Result<CountriesListModel, CountriesListModel> {
+        val result = covidRemoteDataSource.getCountriesList()
+        return result
+    }
+
+    suspend fun getCountryCovidData(countryCode: String): Result<CountryCovidDetailModel, CountryCovidDetailModel> {
+        val result = covidRemoteDataSource.getCountryCovidData(countryCode)
+        return result
+    }
 
     companion object {
         @Volatile
